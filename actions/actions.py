4afb8bc5becee8da1f08fcb82f819a9a0c56ba48
Shahzaib Sajid <shahzaibjanjua25@gmail.com>
2023-09-17 01:28:16 +0500

    initial

---
+++ actions/actions.py
@@ -0,0 +1,96 @@
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import UserUtteranceReverted
+
+class ActionGreet(Action):
+    def name(self) -> Text:
+        return "action_greet"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_greet")
+        return []
+
+class ActionWeather(Action):
+    def name(self) -> Text:
+        return "action_weather"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_weather")
+        return []
+
+class ActionJoke(Action):
+    def name(self) -> Text:
+        return "action_joke"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_joke")
+        return []
+
+class ActionAdvice(Action):
+    def name(self) -> Text:
+        return "action_advice"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_advice")
+        return []
+
+class ActionTellJoke(Action):
+    def name(self) -> Text:
+        return "action_tell_joke"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_tell_joke")
+        return []
+
+class ActionHealthTips(Action):
+    def name(self) -> Text:
+        return "action_health_tips"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_health_tips")
+        return []
+
+class ActionFunFact(Action):
+    def name(self) -> Text:
+        return "action_fun_fact"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_fun_fact")
+        return []
+
+class ActionInspirationalQuote(Action):
+    def name(self) -> Text:
+        return "action_inspirational_quote"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_inspirational_quote")
+        return []
+
+class ActionHistoryFacts(Action):
+    def name(self) -> Text:
+        return "action_history_facts"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_history_facts")
+        return []
+
+class ActionLifeHacks(Action):
+    def name(self) -> Text:
+        return "action_life_hacks"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_life_hacks")
+        return []
+
+# Add more actions for other intents as needed
+
+class ActionFallback(Action):
+    def name(self) -> Text:
+        return "action_fallback"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_template("utter_default", tracker)
+        return [UserUtteranceReverted()]
+
+# Define actions for other intents in a similar manner
